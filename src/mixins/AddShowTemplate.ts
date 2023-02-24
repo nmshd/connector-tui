37b71a4c73eeec1a3484150968ba712a84f362b8
jkoenig <julian.koenig@js-soft.com>
2023-02-24 09:59:30 +0100

    chore: add usecase for empty template

---
+++ src/mixins/AddShowTemplate.ts
@@ -0,0 +1,25 @@
+import { DateTime } from "luxon"
+import qrcode from "qrcode-terminal"
+import { ConnectorTUIBaseConstructor } from "../ConnectorTUIBase"
+
+export function AddCreateAndShowTemplate<TBase extends ConnectorTUIBaseConstructor>(Base: TBase) {
+  return class CreateAndShowTemplate extends Base {
+    public constructor(...args: any[]) {
+      super(...args)
+      this.choices.push({ title: "Create And Show Template", value: this.createAndShowTemplate })
+    }
+
+    public async createAndShowTemplate() {
+      const template = await this.connectorClient.relationshipTemplates.createOwnRelationshipTemplate({
+        content: {},
+        expiresAt: DateTime.now().plus({ days: 2 }).toISO(),
+      })
+
+      const templateId = template.result.id
+      const tokenResponse = await this.connectorClient.relationshipTemplates.createTokenForOwnRelationshipTemplate(templateId)
+      const url = `nmshd://qr#${tokenResponse.result.truncatedReference}`
+      console.log(url)
+      qrcode.generate(url, { small: true })
+    }
+  }
+}
