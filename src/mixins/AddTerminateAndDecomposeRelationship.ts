64393d8a3866312ed49070634b3b86732fb31eb8
Julian König <julian.koenig@js-soft.com>
2024-07-16 13:55:37 +0200

    chore: log errors

--- src/mixins/AddTerminateAndDecomposeRelationship.ts
+++ src/mixins/AddTerminateAndDecomposeRelationship.ts
@@ -12,10 +12,18 @@ export function AddTerminateAndDecomposeRelationship<TBase extends ConnectorTUIB
       if (!relationship) return
 
       console.log(`Terminating relationship ${relationship.id}`)
-      await this.connectorClient.relationships.terminateRelationship(relationship.id)
+      const terminationResult = await this.connectorClient.relationships.terminateRelationship(relationship.id)
+      if (terminationResult.isError) {
+        console.error(`Failed to terminate relationship ${relationship.id}: ${terminationResult.error}`)
+        return
+      }
 
       console.log(`Decomposing relationship ${relationship.id}`)
-      await this.connectorClient.relationships.decomposeRelationship(relationship.id)
+      const decomposeResult = await this.connectorClient.relationships.decomposeRelationship(relationship.id)
+      if (decomposeResult.isError) {
+        console.error(`Failed to terminate relationship ${relationship.id}: ${terminationResult.error}`)
+        return
+      }
     }
   }
 }
