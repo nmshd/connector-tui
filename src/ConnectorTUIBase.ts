ca7c85ed9f85bc3f2227c2b7b2a1d0d233de3ebb
Julian König <julian.koenig@js-soft.com>
2024-07-19 10:30:12 +0200

    chore: allow multiple status

--- src/ConnectorTUIBase.ts
+++ src/ConnectorTUIBase.ts
@@ -15,7 +15,9 @@ export class ConnectorTUIBase {
     protected connectorAddress: string
   ) {}
 
-  protected async selectRelationship(prompt: string, status: ConnectorRelationshipStatus = ConnectorRelationshipStatus.Active): Promise<ConnectorRelationship | undefined> {
+  protected async selectRelationship(prompt: string, ...status: ConnectorRelationshipStatus[]): Promise<ConnectorRelationship | undefined> {
+    if (status.length === 0) status.push(ConnectorRelationshipStatus.Active)
+
     const choices = await this.getRelationshipChoices(status, true)
     if (!choices) return
 
@@ -24,7 +26,9 @@ export class ConnectorTUIBase {
     return recipientsResult.recipient as ConnectorRelationship | undefined
   }
 
-  protected async selectRelationships(prompt: string, status: ConnectorRelationshipStatus = ConnectorRelationshipStatus.Active): Promise<string[] | undefined> {
+  protected async selectRelationships(prompt: string, ...status: ConnectorRelationshipStatus[]): Promise<string[] | undefined> {
+    if (status.length === 0) status.push(ConnectorRelationshipStatus.Active)
+
     const choices = await this.getRelationshipChoices(status, false)
     if (!choices) return
 
@@ -39,7 +43,7 @@ export class ConnectorTUIBase {
     return recipients
   }
 
-  private async getRelationshipChoices(status: ConnectorRelationshipStatus, returnRelationship: boolean) {
+  private async getRelationshipChoices(status: ConnectorRelationshipStatus[], returnRelationship: boolean) {
     const relationshipsResult = await this.connectorClient.relationships.getRelationships({ status })
     if (relationshipsResult.isError) {
       console.error(relationshipsResult.error)
