47f726d1b20fe5dc02dce6f512b3b03f4d7ce36b
Julian König <33655937+jkoenig134@users.noreply.github.com>
2025-05-26 11:45:47 +0200

    Open created templates directly in debug apps (#61)
    * feat: add question if it should be auto opened
    
    * feat: open app in debugger

--- src/mixins/AddShareRequestByTemplate.ts
+++ src/mixins/AddShareRequestByTemplate.ts
@@ -1,4 +1,5 @@
 import { RelationshipTemplateContentJSON, RequestItemGroupJSON, RequestJSON } from "@nmshd/content"
+import { exec } from "child_process"
 import { DateTime } from "luxon"
 import prompts from "prompts"
 import qrcode from "qrcode-terminal"
@@ -234,6 +235,37 @@ export function AddShareRequestByTemplate<TBase extends ConnectorTUIBaseConstruc
       const url = template.result.reference.url
       console.log(url)
       qrcode.generate(url, { small: true })
+
+      const result = await prompts({
+        message: "Do you want to open the link on a connected device?",
+        type: "confirm",
+        name: "open",
+        initial: false,
+      })
+
+      if (!result.open) return
+
+      const androidOrIOS = await prompts({
+        message: "Is the connected device Android or iOS?",
+        type: "select",
+        name: "deviceType",
+        choices: [
+          { title: "Android", value: "Android" },
+          { title: "iOS", value: "iOS" },
+        ],
+      })
+
+      if (!androidOrIOS.deviceType) return
+
+      const command = androidOrIOS.deviceType === "Android" ? `adb shell am start -a android.intent.action.VIEW -d "${url}"` : `xcrun simctl openurl booted "${url}"`
+
+      await new Promise<void>((resolve) => {
+        exec(command, (err, _, __) => {
+          if (err) return console.error(`Error executing command: ${err.message}`)
+
+          resolve()
+        })
+      })
     }
   }
 }
