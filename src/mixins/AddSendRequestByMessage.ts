5ba4d8de475ab50b973508f522849c6fa9417346
Julian König <julian.koenig@js-soft.com>
2025-08-25 10:05:48 +0200

    chore: update wording

--- src/mixins/AddSendRequestByMessage.ts
+++ src/mixins/AddSendRequestByMessage.ts
@@ -277,7 +277,7 @@ export function AddSendRequestByMessage<TBase extends ConnectorTUIBaseConstructo
           type: "select",
           name: "owner",
           choices: [
-            { title: "The other side or a third party", value: "" },
+            { title: "The recipient or the third party", value: "" },
             { title: "The recipient", value: "recipient" },
             { title: "The third party", value: "thirdParty" },
           ],
