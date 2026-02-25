8fabe61e180160c2d704e3b06118a1f39bc02619
britsta <britta.stallknecht@js-soft.com>
2026-02-25 10:25:58 +0100

    chore: remove audit exclude

--- .ci/build.sh
+++ .ci/build.sh
@@ -5,5 +5,5 @@ npm ci
 npm run lint:prettier
 npm run lint:eslint
 npx license-check --ignoreRegex "@nmshd/*"
-npx better-npm-audit audit --exclude 1113371
+npx better-npm-audit audit
 npm run build
